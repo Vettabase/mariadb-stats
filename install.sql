d51759c74a29fa313e2f06e2166b20499b000a42
Federico Razzoli <federico.razzoli@vettabase.com>
2024-08-19 03:17:24 +0100

    Add geometric_mean() and weighted_geometric_mean() stubs

---
+++ install.sql
@@ -0,0 +1,57 @@
+/*
+    This file is part of MariaDB Statistics Library.
+
+    MariaDB Statistics Library is free software: you can redistribute it and/or modify it under the terms of the
+    GNU Affero General Public License as published by the Free Software Foundation, version 3 of the License.
+
+    MariaDB Statistics Library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+    without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License along with MariaDB Statistics Library.
+    If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
+DELIMITER ||
+
+    CREATE OR REPLACE AGGREGATE FUNCTION geometric_mean(item FLOAT)
+        RETURNS FLOAT
+    BEGIN
+        DECLARE part FLOAT DEFAULT 1;
+        DECLARE weight_sum FLOAT DEFAULT 0;
+        DECLARE CONTINUE HANDLER FOR NOT FOUND
+            RETURN POW(part, 1 / weight_sum);
+        LOOP
+            FETCH GROUP NEXT ROW;
+            SET part := part * item;
+            SET weight_sum := weight_sum + 1;
+        END LOOP;
+    END;
+
+||
+DELIMITER ;
+
+
+DELIMITER ||
+
+    CREATE OR REPLACE AGGREGATE FUNCTION weighted_geometric_mean(
+            item FLOAT,
+            weight FLOAT
+        )
+        RETURNS FLOAT
+    BEGIN
+        DECLARE part FLOAT DEFAULT 1;
+        DECLARE weight_sum FLOAT DEFAULT 0;
+        DECLARE CONTINUE HANDLER FOR NOT FOUND
+            RETURN POW(part, 1 / weight_sum);
+        LOOP
+            FETCH GROUP NEXT ROW;
+            SET part := part * POW(item, weight);
+            SET weight_sum := weight_sum + weight;
+        END LOOP;
+    END;
+
+||
+DELIMITER ;
+
---
+++ test.sql
@@ -0,0 +1,37 @@
+/*
+    This file is part of MariaDB Statistics Library.
+
+    MariaDB Statistics Library is free software: you can redistribute it and/or modify it under the terms of the
+    GNU Affero General Public License as published by the Free Software Foundation, version 3 of the License.
+
+    MariaDB Statistics Library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+    without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License along with MariaDB Statistics Library.
+    If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
+-- geometric_mean()
+
+CREATE OR REPLACE TEMPORARY TABLE t (i FLOAT);
+-- Expected Result: NULL
+SELECT geometric_mean(i) FROM t;
+
+CREATE OR REPLACE TEMPORARY TABLE t (i FLOAT);
+INSERT INTO t VALUES (1.05), (1.03), (1.06), (1.02), (1.04);
+-- Expected Result: 1.0399
+SELECT geometric_mean(i) FROM t;
+
+
+-- weighted_geometric_mean()
+
+CREATE OR REPLACE TEMPORARY TABLE t (i FLOAT, w FLOAT);
+-- Expected Result: NULL
+SELECT weighted_geometric_mean(i, w) FROM t;
+
+CREATE OR REPLACE TEMPORARY TABLE t (i FLOAT, w FLOAT);
+INSERT INTO t VALUES (1, 2), (2, 5), (3, 6), (4, 4), (5, 3);
+-- Expected Result: 2.77748
+SELECT weighted_geometric_mean(i, w) FROM t;
